734e5e725dcfe0802d870204b524a6646c40c17d
webianks <webianks@gmail.com>
2016-12-12 12:36:00 +0530

    Taking ScreenShots

---
+++ easy_feedback/src/main/java/com/webianks/easy_feedback/TakingScreenshot.java
@@ -0,0 +1,57 @@
+package com.webianks.easy_feedback;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.view.View;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+/**
+ * Created by R Ankit on 12-12-2016.
+ */
+
+public class TakingScreenshot {
+
+
+    public static void takeScreenshot(Context context) {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = ((Activity) context).getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile, context);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+
+    private static void openScreenshot(File imageFile, Context context) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        context.startActivity(intent);
+    }
+}
